1e49bd7929f3f99a10c43d67e4a02735fbd75fc0
Dhruv Srivastava <dhruvsri37@gmail.com>
2025-06-22 19:25:15 +0530

    upodated

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,19 +15,16 @@ provider "google" {
 
 resource "google_project_service" "cloud_run_api" {
   service = "run.googleapis.com"
-  
   disable_dependent_services = true
 }
 
 resource "google_project_service" "container_registry_api" {
   service = "containerregistry.googleapis.com"
-  
   disable_dependent_services = true
 }
 
 resource "google_project_service" "cloud_build_api" {
   service = "cloudbuild.googleapis.com"
-  
   disable_dependent_services = true
 }
 
@@ -39,32 +36,26 @@ resource "google_cloud_run_service" "mobilenet_classifier" {
     spec {
       containers {
         image = var.image_url
-        
+
         ports {
           container_port = 8000
         }
-        
+
         resources {
           limits = {
             cpu    = "1000m"
             memory = "1Gi"
           }
         }
-        
-        env {
-          name  = "PORT"
-          value = "8000"
-        }
       }
-      
-      # Set timeout for longer model loading
+
       timeout_seconds = 300
     }
-    
+
     metadata {
       annotations = {
-        "autoscaling.knative.dev/maxScale" = "10"
-        "run.googleapis.com/cpu-throttling" = "false"
+        "autoscaling.knative.dev/maxScale"      = "10"
+        "run.googleapis.com/cpu-throttling"     = "false"
       }
     }
   }
